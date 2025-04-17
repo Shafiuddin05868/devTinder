34b4bfb83a3e06d71f4b34a8a1540c6507b5b031
Shafiuddin Chowdury <shafiuddin05868@gmail.com>
2025-04-17 20:55:45 +0600

    method tests

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,20 @@ const express = require('express')
 
 const app = express()
 
-app.get("/", (req, res)=>{
-    res.send("This is my home!")
-});
+// app.use("/", (req, res)=>{
+//     res.send("This is my home!")
+// });
 
-app.get("/about", (req, res)=>{
-    res.send("This is about page")
-});
+app.get('/user', (req, res)=>{
+    res.send({name: "Shafiuddin Chowdury", email: "shafiuddin05868@yendex.com"})
+})
+app.post('/user', (req, res)=>{
+    res.send("data saved successfully")
+})
+
+app.get('/user',(req,res)=>{
+    res.send("lalalal")
+})
 
 app.listen(3000, ()=> {
     console.log("The server is running on port 3000")
