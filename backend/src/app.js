feab8858390fa24a8228244e42c186237edaacb6
Shafiuddin Chowdury <shafiuddin05868@gmail.com>
2025-04-18 23:59:28 +0600

    practice params and queries

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,16 +6,17 @@ const app = express()
 //     res.send("This is my home!")
 // });
 
-app.get('/user', (req, res)=>{
-    res.send({name: "Shafiuddin Chowdury", email: "shafiuddin05868@yendex.com"})
-})
-app.post('/user', (req, res)=>{
-    res.send("data saved successfully")
+// app.get('/user', (req, res)=>{
+//     res.send({name: "Shafiuddin Chowdury", email: "shafiuddin05868@yendex.com"})
+// })
+app.get('/user/:id/:public', (req, res)=>{
+    const {id, public} = req.params;
+    res.send(id + public)
 })
 
-app.get('/user',(req,res)=>{
-    res.send("lalalal")
-})
+// app.get('/user',(req,res)=>{
+//     res.send("lalalal")
+// })
 
 app.listen(3000, ()=> {
     console.log("The server is running on port 3000")
