f673fc556b89bad317a02b81696af8d20a17ebf5
Shafiuddin Chowdury <shafiuddin05868@gmail.com>
2025-04-19 21:46:57 +0600

    playing with route handlers

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,22 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-// app.use("/", (req, res)=>{
-//     res.send("This is my home!")
-// });
-
-// app.get('/user', (req, res)=>{
-//     res.send({name: "Shafiuddin Chowdury", email: "shafiuddin05868@yendex.com"})
-// })
-app.get('/user/:id/:public', (req, res)=>{
-    const {id, public} = req.params;
-    res.send(id + public)
+app.use('/user', (req, res, next)=>{
+    console.log("hello 1");
+    next()
 })
 
-// app.get('/user',(req,res)=>{
-//     res.send("lalalal")
-// })
+app.use('/user', (req, res, next)=>{
+    next()
+    console.log("hello 2")
+    res.send("hello 2")
+})
+app.use('/user', (req, res, next)=>{
+    console.log("hello 3")
+})
 
-app.listen(3000, ()=> {
-    console.log("The server is running on port 3000")
-});
+app.listen(3000, ()=>{
+    console.log("The server is running at port 3000");
+    
+})
