357581c484b37953d1a6b4a7730eda6d11acee4b
Shafiuddin Chowdury <shafiuddin05868@gmail.com>
2025-04-27 01:08:35 +0600

    saving static data to the database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,27 @@
 import express from "express";
 import { databaseConnect } from "./config/database.js";
+import { User } from "./models/user.js";
 
 const app = express();
 
-(() => { //connect database and start server
+app.post("/signUp", async (req, res) => {
+  const user = new User({
+    name: "Shafiuddin Chowdury",
+    email: "shafiuddin05868@yandex.com",
+    password: "dkf84359348AKDSJF@$##@",
+    phone: 8801799979556,
+    age: 23,
+    gender: "male",
+  });
+  try {
+    await user.save();
+    res.send("user create successfully");
+  } catch (err) {
+    res.status(400).send("failed to save user", err);
+  }
+});
+//connect database and start server
+(() => {
   try {
     databaseConnect();
     app.listen(3000, () => {
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 
-export const User = new mongoose.Schema({
-    name: string,
-    email: string,
-    password: string,
-    phone: number,
-    age: number,
-    gender: string
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    phone: Number,
+    age: Number,
+    gender: String
 })
+
+export const User = mongoose.model('User', userSchema)
