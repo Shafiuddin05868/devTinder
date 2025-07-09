9a93fe37547eb070b9819059f1d095863c08ae16
Shafiuddin Chowdury <shafiuddin05868@gmail.com>
2025-07-09 20:04:33 +0600

    feat: add password update API endpoint

--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { userAuth } from "../middlewares/auth.js";
+import bcrypt from "bcrypt";
 import { validateProfileUpdate } from "../utils/validate.js";
 
 const profileRouter = express.Router();
@@ -20,7 +21,31 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     const userInfo = req.user;
     Object.keys(req.body).forEach((key) => (userInfo[key] = req.body[key]));
     await userInfo.save();
-    res.status(200).send({message:"Profile updated successfully",data: userInfo});
+    res
+      .status(200)
+      .send({ message: "Profile updated successfully", data: userInfo });
+  } catch (err) {
+    res.status(500).send("internal server error: " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    const isMatch = bcrypt.compareSync(req.body.oldPassword, user.password);
+    if (!isMatch) {
+      return res.status(400).send({ message: "password is incorrect" });
+    }
+    bcrypt.hash(req.body.newPassword, 10, async (err, hash) => {
+      if (err) {
+        throw new Error("Internal server error: " + err.message);
+      }
+      user.password = hash;
+      await user.save();
+      res
+        .status(200)
+        .send({ message: "Password updated successfully", data: user });
+    });
   } catch (err) {
     res.status(500).send("internal server error: " + err.message);
   }
