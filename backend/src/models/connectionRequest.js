fb27f92bf13280e8a307f4dac11c702f76fc67eb
Shafiuddin Chowdury <shafiuddin05868@gmail.com>
2025-07-24 19:33:00 +0600

    feat: add missing ref for senderId in connectionRequest schema

--- backend/src/models/connectionRequest.js
+++ backend/src/models/connectionRequest.js
@@ -5,6 +5,7 @@ const connectionRequestionSchema = new Schema(
     senderId: {
       type: Schema.Types.ObjectId,
       required: true,
+      ref: "User"
     },
     receiverId: {
       type: Schema.Types.ObjectId,
