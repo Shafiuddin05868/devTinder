a61d72a03b0009876ad50bffad507d6e1bb8ad02
Shafiuddin Chowdury <shafiuddin05868@gmail.com>
2025-07-24 19:29:35 +0600

    feat: create and add userRouter

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,7 @@ dotenv.config()
 import { authRouter } from "./routes/auth.js"
 import { profileRouter } from "./routes/profile.js"
 import { requestRouter } from "./routes/request.js"
+import { userRouter } from "./routes/user.js"
 
 const app = express()
 
@@ -18,6 +19,7 @@ app.use(cookieParser())
 app.use("/", authRouter)
 app.use("/", profileRouter)
 app.use("/", requestRouter)
+app.use("/", userRouter)
 
 //connect database and start server
 ;(async () => {
