ea3f6051481b08313f4860660a4d6237af900ee3
Shafiuddin Chowdury <shafiuddin05868@gmail.com>
2025-07-25 03:49:11 +0600

    fix: add missing ref for receiverId in connectionRequest schema and improve user request handling

--- backend/src/models/connectionRequest.js
+++ backend/src/models/connectionRequest.js
@@ -10,6 +10,7 @@ const connectionRequestionSchema = new Schema(
     receiverId: {
       type: Schema.Types.ObjectId,
       required: true,
+      ref: "User"
     },
     status: {
       type: String,
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -5,13 +5,55 @@ const userRouter = express.Router();
 
 userRouter.get("/user/requests", userAuth, async (req, res) => {
   try {
-    const user  = req.user;
-    const requests = await connectionRequest.find({
+    const user = req.user;
+    const requests = await connectionRequest
+      .find({
         receiverId: user.id,
-        status: "interested"
-    }).populate("senderId", "name photoUrl age")
+        status: "interested",
+      })
+      .populate("senderId", "name photoUrl age");
+    const data = requests.map((request) => ({
+      requestId: request._id,
+      senderId: request.senderId,
+    }));
 
-    res.status(200).json({ message: requests.length > 0 ? "Requests fetched successfully" : "No request found", data: requests || [] })
+    res
+      .status(200)
+      .json({
+        message:
+          requests.length > 0
+            ? "Requests fetched successfully"
+            : "No request found",
+        data,
+      });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "ERROR: " + error.message || "Unknown error" });
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    const connections = await connectionRequest
+      .find({
+        $or: [
+          { senderId: user.id, status: "accepted" },
+          { receiverId: user.id, status: "accepted" },
+        ],
+      })
+      .populate("senderId", "name photoUrl age")
+      .populate("receiverId", "name photoUrl age");
+    const data = connections.map((connection) => connection.senderId || connection.receiverId);
+
+    res.status(200).json({
+        message:
+          connections.length > 0
+            ? "Connections fetched successfully"
+            : "No connection found",
+        data,
+    })
   } catch (error) {
     res
       .status(400)
