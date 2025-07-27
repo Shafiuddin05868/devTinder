cda92784374c399db9ef449a52f56cc1952c4018
Shafiuddin Chowdury <shafiuddin05868@gmail.com>
2025-07-27 19:36:00 +0600

    feat: add login

--- frontend/app/routes/login.tsx
+++ frontend/app/routes/login.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { apiUrl } from "~/utils/api";
 
 const login = () => {
-  return (
-    <div>login</div>
-  )
-}
+    const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const email = event.currentTarget.email.value;
+    const password = event.currentTarget.password.value;
+    console.log(email, password);
+    fetch(apiUrl("/login"), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ email, password }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
 
-export default login
+  return (
+    //create a login page with email and password
+
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <label>Email:</label>
+        <input type="email" name="email" />
+        <label>Password:</label>
+        <input type="password" name="password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default login;
