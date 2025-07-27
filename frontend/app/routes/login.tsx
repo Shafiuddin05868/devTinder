57e01e8961d5ecdd59286e6aa7c28c859a25795f
Shafiuddin Chowdury <shafiuddin05868@gmail.com>
2025-07-27 19:58:49 +0600

    refactor: improve the login ui

--- frontend/app/routes/login.tsx
+++ frontend/app/routes/login.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import { apiUrl } from "~/utils/api";
 
 const login = () => {
-    const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const email = event.currentTarget.email.value;
     const password = event.currentTarget.password.value;
-    console.log(email, password);
     fetch(apiUrl("/login"), {
       method: "POST",
       headers: {
@@ -24,15 +23,17 @@ const login = () => {
   return (
     //create a login page with email and password
 
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <label>Email:</label>
-        <input type="email" name="email" />
-        <label>Password:</label>
-        <input type="password" name="password" />
-        <button type="submit">Login</button>
-      </form>
+    <div className="w-screen h-screen bg-black flex justify-center items-center">
+      <div className="w-full max-w-md p-8 space-y-3 rounded-xl border-2">
+        <h1 className="text-2xl font-bold">Login</h1>
+        <form onSubmit={handleLogin}>
+          <label htmlFor="email">Email:</label>
+          <input required className="border ml-2 my-2 rounded-lg px-3 py-1" placeholder="Enter you email" type="email" id="email" name="email" /> <br />
+          <label htmlFor="password">Password:</label>
+          <input required className="border ml-2 my-2 rounded-lg px-3 py-1" placeholder="Enter you password" type="password" id="password" name="password" /> <br />
+          <button className="py-2 px-3 border rounded-lg " type="submit">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
